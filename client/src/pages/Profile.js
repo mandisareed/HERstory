5e3aa230afc738e721d830b203d42f72b9e7b78d
Diachn <Diiianachen@gmail.com>
2020-08-19 19:04:25 -0700

    Created a notepad for the users to takes notes with

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, useReducer } from "react";
 import API from "./../utils/API";
 import Container from "../components/Container";
 import { Link } from "react-router-dom";
 import { useAuth } from "../utils/auth";
 import "./Profile.css";
+import AddNote from '../components/AddNote';
+import NoteList from '../components/NoteList';
+import EditNote from '../components/EditNote';
+import NotesContext from '../pages/context';
+import notesReducer from '../pages/reducer';
+
 function Profile() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const { user } = useAuth();
+  const initialState = useContext(NotesContext);
+  const [state, dispatch] = useReducer(notesReducer, initialState);
 
   useEffect(() => {
     API.getUser(user.id).then((res) => {
@@ -21,16 +29,29 @@ function Profile() {
     <body className= "profileBackground">
       
     <Container>
-      <h1>On the profile page!</h1>
+      <h1>My Profile</h1>
       <p>Username: {username}</p>
       <p>Email: {email}</p>
       <Link to="/">Go home</Link>
     
     </Container>
+    <NotesContext.Provider value={{ state, dispatch }}>
+
+      {state.currentNote === null ? (
+        <div>
+          <AddNote />
+          <NoteList />
+        </div>
+      ) : (
+        <EditNote />
+      )}
+    </NotesContext.Provider>
+  
     </body>
   );
   
 }
+
 
 
 export default Profile;
---
+++ client/src/pages/context.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotesContext = React.createContext({
+  currentNote: null,
+  notes: [
+    { id: 1, text: 'Study more about Rosa Parks since I got a question wrong about her during the quiz.' },
+    { id: 2, text: 'Find out who Amelia Earhart is.' },
+    { id: 3, text: 'Find out why Clara Barton founded the American Red Cross.' }
+  ]
+})
+
+export default NotesContext;
