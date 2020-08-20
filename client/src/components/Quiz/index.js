3cb81c85c795aed6dc49eeb84406adb29f4dea32
Mandisa Reed <mandisa.reed@gmail.com>
2020-08-19 21:33:10 -0700

    created static results card

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -6,6 +6,7 @@ import CardContent from "@material-ui/core/CardContent";
 // import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import QuizInstructions from "./instructions";
+import QuizResults from "./results";
 
 const useStyles = makeStyles({
   root: {
@@ -51,6 +52,8 @@ function QuizCard() {
         </p>
       </CardContent>
     </Card>
+
+    <QuizResults />
     </div>
   );
 }
